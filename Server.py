fd90c18a1b9b0653d2fd103e7d23781ec067eb67
Erin <dragushaerin@gmail.com>
2022-10-05 13:36:13 -0700

    Working Server Lab 2

--- Server.py
+++ Server.py
@@ -4,6 +4,28 @@ BUF_SIZE = 1024
 HOST = ''
 PORT = 12345
 board = [[["_" for _ in range(4)] for _ in range(4)] for _ in range(4)]
+turn = 1
+
+
+def getBoard():
+	boardString = ""
+	for level in board:
+		for row in level:
+			for col in row:
+				boardString += col
+			boardString += "\n"
+		boardString += "\n"
+	return boardString
+
+def displayError(sc):
+	sc.sendall("E\n".encode())
+
+def displaySuccess(sc):
+	sc.sendall("O\n".encode())
+
+def displayMessage(sc, message):
+	sc.sendall((message+"\n").encode())
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: # TCP socket
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Details later
 	sock.bind((HOST, PORT)) # Claim messages sent to port "PORT"
@@ -13,47 +35,66 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: # TCP socket
 		sc, sockname = sock.accept() # Wait until a connection is established
 		with sc:
 			print('Client:', sc.getpeername()) # Dest. IP and p
-			responseMessage = ''
 			data = sc.recv(BUF_SIZE) # recvfrom not needed since address is known
 			decodedData = data.decode()
 			print(list(decodedData))
-			result = ''
+			COMMAND = decodedData[0]
 
-			if decodedData[0] == 'G':
-				for row in board:
-					result += '\n'
-					for ro in row:
-						result += '\n'
-						for r in ro:
-							result += r
-				result += '\n'
-			elif decodedData[0] == 'P':
+			if COMMAND == 'G':
+				displayMessage(sc, getBoard())
+			elif COMMAND == 'P':
 
-				if not decodedData[1].isnumeric() or not decodedData[2].isnumeric() or not decodedData[3].isnumeric():
-					responseMessage = "Invalid input!"
+				if len(decodedData) != 6:
+					displayError(sc)
+					print("Invalid number of inputs!")
+					continue
+
+				
+				level = decodedData[1]
+				row = decodedData[2]
+				col = decodedData[3]
+				inputRecord = decodedData[4]
+
+				if not level.isnumeric() or not row.isnumeric() or not col.isnumeric() or not inputRecord.isnumeric():			
+					displayError(sc)
+					print("One of the arguments is NaN")
+					continue
+
+				level = int(level)	
+				row = int(row)	
+				col = int(col)
+				inputRecord = int(inputRecord)
+
+				if inputRecord != turn:
+					displayError(sc)
+					print("Not your turn!")
+					continue	
+
+				try:
+					board[level][row][col]
+				except IndexError:
+					displayError(sc)
+					print("Out of bounds!")
+					continue
+
+				if board[level][row][col] != '_':
+					displayError(sc)
+					print("Section occupied!")
+					continue
+
+				board[level][row][col] = str(inputRecord)
+				displaySuccess(sc)
+				print("Sucessfully placed on board!")
+
+				if turn == 3:
+					turn = 1
 				else:
-
-					inputRecord = decodedData[4]
-					firstPosition = int(decodedData[1])
-					secondPosition = int(decodedData[2])
-					thirdPosition = int(decodedData[3])
-
-					if firstPosition > 3:
-						responseMessage += "First position is out of bounds!"
-
-					if secondPosition > 3:
-						responseMessage += "Second position is out of bounds!"
-
-					if thirdPosition > 3:
-						responseMessage += "Third position is out of bounds!"
-
-					if responseMessage == '':
-						if board[firstPosition][secondPosition][thirdPosition] == "_":
-							board[firstPosition][secondPosition][thirdPosition] = inputRecord
-							responseMessage = "OK"
-						else:
-							responseMessage = "ERROR! This section is occupied!"
-
-			print(responseMessage)
-			sc.sendall(result.encode())
-
+					turn+=1
+			elif COMMAND == 'C':
+				turn = 1
+				board = [[["_" for _ in range(4)] for _ in range(4)] for _ in range(4)]
+				displaySuccess(sc)
+				print("Board cleared!")
+			else:
+				displayError(sc)
+				print("Unknown command!")
